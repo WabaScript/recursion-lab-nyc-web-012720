6c6258648a57331dd6fde26c14d5cd4dffdf27bc
WabaScript <rjwahba@gmail.com>
2020-08-22 16:09:14 -0400

    pal

--- index.js
+++ index.js
@@ -19,18 +19,6 @@ printString('tacotuesday');
 // ## Additional string challenges
 
 // 1. Write out a recursive function to reverse a string.
-// 2. Write out a recursive function to see if a word is a palindrome.
-
-// ## Array challenges
-
-// Arrays are another type of recursion-happy data structure. This is because, similar to the string–substring relationship, the properties of an array can be thought of as a series of sub-arrays or, in the context of our `printString()` code above, a combination of sub-arrays and a final element.
-
-// 1. Given an array and an index, write a recursive function to add up the elements of an array.
-// 2. Write a recursive function to find the largest integer in an array.
-// 3. Write out a function to see if an array includes a given element.
-
-
-// 1. Write out a recursive function to reverse a string.
 function reverseString(string) {
     if (string.length > 0){
       let rev = string[string.length - 1];
@@ -41,3 +29,26 @@ function reverseString(string) {
     }
 };
 reverseString('big cow party here');
+
+// 2. Write out a recursive function to see if a word is a palindrome.
+function checkPalindrome(str) {
+  string = str.toLowerCase()
+  len = string.length
+  if (len <= 1) {
+    return true;
+  }
+  if (string[0] === string[len - 1]){
+    return checkPalindrome(string.substring(1,len-1))
+  } else {
+    return false;
+  }
+}
+checkPalindrome('pooooooop')
+
+// ## Array challenges
+
+// Arrays are another type of recursion-happy data structure. This is because, similar to the string–substring relationship, the properties of an array can be thought of as a series of sub-arrays or, in the context of our `printString()` code above, a combination of sub-arrays and a final element.
+
+// 1. Given an array and an index, write a recursive function to add up the elements of an array.
+// 2. Write a recursive function to find the largest integer in an array.
+// 3. Write out a function to see if an array includes a given element.
